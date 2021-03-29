f67b0440d08066446df0eedf31e1df043f785a5f
skmedix <skmedixs@gmail.com>
2021-03-29 19:27:10 +0200

    :alien: Beta.16

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,10 +1,10 @@
-import { extend } from 'flarum/extend';
-import Application from 'flarum/Application';
+import { extend } from 'flarum/common/extend';
+import ForumApplication from 'flarum/forum/ForumApplication';
 
 import CustomFooter from './components/CustomFooter';
 
 app.initializers.add('fof-custom-footer', () => {
-    extend(Application.prototype, 'mount', () => {
+    extend(ForumApplication.prototype, 'mount', () => {
         const footer = document.createElement('div');
 
         m.mount(document.body.appendChild(footer), CustomFooter);
